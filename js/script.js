2d1ac58e0c72b3692806bedc499db5cc10718800
Joanna Smukala <joasmu@wp.pl>
2018-04-08 20:32:10 +0200

    dodanie plików na github

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 var a = prompt ("liczba a");
 var b = prompt ("liczba b");
-var  = (a * a) - (2 * a * b) - (b * b)
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var result = (a * a) - (2 * a * b) - (b * b)
+
